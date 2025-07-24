73e07037fcd81a82513a6695448d75c62a11666c
Robert Ancell <robert.ancell@canonical.com>
2025-07-24 14:25:43 +1200

    Initial commit

---
+++ run.sh
@@ -0,0 +1,1 @@
+MESA_DEBUG=1 G_MESSAGES_DEBUG=all GDK_DEBUG=gl-debug flutter run --local-engine-src-path ~/git/flutter/flutter/engine/src/ --local-engine host_debug --local-engine-host host_debug -v
