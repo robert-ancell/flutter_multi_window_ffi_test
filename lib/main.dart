82c8869f08cf2b8794c595dca8c482d28722b3b1
Robert Ancell <robert.ancell@canonical.com>
2025-07-25 15:41:43 +1200

    Add more GTK API

--- lib/main.dart
+++ lib/main.dart
@@ -64,6 +64,67 @@ class GtkWindow extends GtkWidget {
   void setTitle(String title) {
     _gtkWindowSetTitle(pointer, title.toNativeUtf8());
   }
+
+  @Native<Void Function(Pointer, Int, Int)>(
+    symbol: 'gtk_window_set_default_size',
+  )
+  external static void _gtkWindowSetDefaultSize(
+    Pointer window,
+    int width,
+    int height,
+  );
+  void setDefaultSize(int width, int height) {
+    _gtkWindowSetDefaultSize(pointer, width, height);
+  }
+
+  //  @Native<Void Function(Pointer, Pointer<ffi.Utf8>)>(
+  //    symbol: 'gtk_window_set_geometry_hints',
+  //  )
+  //  external static void _gtkWindowSetGeometryHints(
+  //    Pointer window,
+  //    Pointer geometryWidget,
+  //    geometry,
+  //    int geometryMask,
+  //  );
+  //  void setGeometryHints(Pointer window, geometry, int geometryMask) {
+  //    _gtkGeometryHints(pointer, null, geometry, geometryMask);
+  //  }
+
+  @Native<Void Function(Pointer, Int, Int)>(symbol: 'gtk_window_resize')
+  external static void _gtkWindowResize(Pointer window, int width, int height);
+  void resize(int width, int height) {
+    _gtkWindowResize(pointer, width, height);
+  }
+
+  @Native<Void Function(Pointer)>(symbol: 'gtk_window_maximize')
+  external static void _gtkWindowMaximize(Pointer window);
+  void maximize() {
+    _gtkWindowMaximize(pointer);
+  }
+
+  @Native<Void Function(Pointer)>(symbol: 'gtk_window_unmaximize')
+  external static void _gtkWindowUnmaximize(Pointer window);
+  void unmaximize() {
+    _gtkWindowUnmaximize(pointer);
+  }
+
+  @Native<Bool Function(Pointer)>(symbol: 'gtk_window_is_maximized')
+  external static bool _gtkWindowIsMaximized(Pointer window);
+  bool isMaximized() {
+    return _gtkWindowIsMaximized(pointer);
+  }
+
+  @Native<Void Function(Pointer)>(symbol: 'gtk_window_iconify')
+  external static void _gtkWindowIconify(Pointer window);
+  void iconify() {
+    _gtkWindowIconify(pointer);
+  }
+
+  @Native<Void Function(Pointer)>(symbol: 'gtk_window_deiconify')
+  external static void _gtkWindowDeiconify(Pointer window);
+  void deiconify() {
+    _gtkWindowDeiconify(pointer);
+  }
 }
 
 @Native<Bool Function(Int64)>(symbol: 'InternalFlutterLinux_HasTopLevelWindows')
@@ -104,7 +165,9 @@ class _MyHomePageState extends State<MyHomePage> {
                 var engineId = PlatformDispatcher.instance.engineId!;
                 var viewId = _createRegularWindow(engineId);
                 var window = GtkWindow(_getGtkWindow(engineId, viewId));
-                //window.present();
+                window.setTitle("Multi-Window FFI Test");
+                window.setDefaultSize(600, 600);
+                window.present();
               },
               child: Text('CreateWindow'),
             ),
